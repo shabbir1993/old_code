e8ce908f85977559986bd8233aaec3affee83463
kevinsu22 <hang.k.su@gmail.com>
2013-04-17 13:17:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pcms::Application.config.secret_token = '604b80e6dd189ae120fd0634879ebc4e481da73866ff69aee1dd9fabcc16b40468173c2c6f0eed72b4f049ff1f6c3ff0aa83a50485fe889c5ffd6b7df3c42420'
---
+++ test/integration/authentication_integration_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper.rb'
+
+describe "Authentication integration" do
+  it "works" do
+    (1+1).must_equal 2
+  end
+end
