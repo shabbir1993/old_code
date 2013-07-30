52a49f68e45d491a4e21049b7269a06c2e08b5fc
Hang Su <hang.k.su@gmail.com>
2013-07-30 01:29:58 -0500

    fix movement bug, add pagination to movements

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,10 +1,10 @@
 class HistoryController < ApplicationController
   def film_movements
-    @film_movements = FilmMovement.all
+    @film_movements = FilmMovement.page(params[:page])
   end
 
   def fg_film_movements
-    @fg_film_movements = FilmMovement.fg
+    @fg_film_movements = FilmMovement.fg.page(params[:page])
   end
 end
 
