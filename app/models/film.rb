6577576b13d182e6c41bba987eea8d2de764a554
Hang Su <hang.k.su@gmail.com>
2013-12-15 15:38:16 -0600

    polish films export

--- app/models/film.rb
+++ app/models/film.rb
@@ -161,9 +161,9 @@ class Film < ActiveRecord::Base
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
-      csv << %w(Serial Width Length Shelf SO Phase Note)
-      all.includes(:sales_order).each do |f|
-        csv << [f.serial, f.width, f.length, f.shelf, f.sales_order_code, f.phase, f.note]
+      csv << %w(Serial Width Length Area Shelf SO Phase Note)
+      all.each do |f|
+        csv << [f.serial, f.width, f.length, f.area, f.shelf, f.sales_order_code, f.phase, f.note]
       end
     end
   end
