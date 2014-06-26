bb5e6baf497f9396dd965e38dbe739fd410f67ab
Hang Su <hang.k.su@gmail.com>
2014-06-26 15:14:10 -0700

    close #43 change admin-only edit fields

--- app/helpers/master_films_helper.rb
+++
@@ -1,5 +0,0 @@
-module MasterFilmsHelper
-  def serial_input_if_admin(form)
-    form.text_field :serial if current_user.admin?
-  end
-end
--- app/helpers/sales_orders_helper.rb
+++ app/helpers/sales_orders_helper.rb
@@ -1,8 +1,4 @@
 module SalesOrdersHelper
-  def code_input_if_admin(form)
-    form.text_field :code, label: "SO#" if current_user.admin?
-  end
-
   def phase_label_class(film)
     case film.phase
     when "wip"
