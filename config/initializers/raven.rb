ab6f457b64c082db8f6c0b6e41d694421e5199aa
Hang Su <hang.k.su@gmail.com>
2014-09-20 22:21:11 -0700

    only use sentry raven in production

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,5 +2,6 @@
 require 'raven'
 
 Raven.configure do |config|
-    config.dsn = 'https://45d47ab6137849ffa3cfe9195488d1ad:b25e7b2d40dc483485606f126bbd6ff7@app.getsentry.com/17304'
+  config.dsn = 'https://45d47ab6137849ffa3cfe9195488d1ad:b25e7b2d40dc483485606f126bbd6ff7@app.getsentry.com/17304'
+  config.environments = %w[ production ]
 end
