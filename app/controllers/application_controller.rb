ea806deabb55b19c61a7e8da18d2131bfdf11307
hangsu <hang.k.su@gmail.com>
2015-02-17 11:44:54 -0800

    add new IP address for Maile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   VALID_IPS = ["66.226.220.106",   #PI Dallas
+               "66.226.207.10",
                "120.33.232.194",   #PE Fujian
                "127.0.0.1"]        #localhost
 
