b078e30144ccad5a010ea009399d1318708c8f57
hangsu <hang.k.su@gmail.com>
2015-08-23 07:47:19 -0700

    change due notation on calendar

--- app/helpers/job_dates_helper.rb
+++ app/helpers/job_dates_helper.rb
@@ -7,11 +7,11 @@ module JobDatesHelper
       end
     elsif job_date.step == "due" && job_date.job_order.supermarket?
       content_tag :span, class: "#{shared_classes} label-warning" do
-        "Due"
+        "SM"
       end
     elsif job_date.step == "due"
       content_tag :span, class: "#{shared_classes} label-danger" do
-        "Due"
+        "FG"
       end
     else
       content_tag :span, class: "#{shared_classes} label-default" do
