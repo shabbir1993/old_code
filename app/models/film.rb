655f8d9a1577914216eb2f9e8d2039a1cca7e4a8
hangsu <hang.k.su@gmail.com>
2016-02-06 11:00:14 -0800

    split fg between recent and archive

--- app/models/film.rb
+++ app/models/film.rb
@@ -28,8 +28,8 @@ class Film < ActiveRecord::Base
   scope :join_dimensions, -> { joins('LEFT OUTER JOIN dimensions ON dimensions.film_id = films.id').uniq }
   scope :join_master_films, -> { joins('INNER JOIN master_films ON master_films.id = films.master_film_id') }
   scope :active, -> { where(deleted: false)
-                     .join_master_films
-                     .merge(MasterFilm.function_not(:test)) }
+                        .join_master_films
+                        .merge(MasterFilm.function_not(:test)) }
   scope :deleted, -> { where(deleted: true) }
   scope :not_deleted, -> { where(deleted: false) }
   scope :has_shelf, -> { where("shelf <> ''") }
@@ -37,7 +37,7 @@ class Film < ActiveRecord::Base
   scope :small, ->(cutoff) { join_dimensions.merge(Dimension.small(cutoff)) }
   scope :text_search, ->(query) { reorder('').search(query) }
   scope :formula_like, ->(formula) { join_master_films
-                                    .merge(MasterFilm.formula_like(formula)) }
+                                       .merge(MasterFilm.formula_like(formula)) }
   scope :order_by, ->(col, dir) { order("#{col} #{dir}") }
   scope :width_greater_than, ->(n) { join_dimensions.merge(Dimension.min_width(n)) }
   scope :length_greater_than, ->(n) { join_dimensions.merge(Dimension.min_length(n)) }
@@ -45,10 +45,10 @@ class Film < ActiveRecord::Base
   scope :serial_date_after, ->(date) { join_master_films.merge(MasterFilm.serial_date_after(date)) }
 
   include PgSearch
-  pg_search_scope :search, 
-    against: [:serial, :note, :shelf, :phase], 
-    using: { tsearch: { prefix: true } },
-    associated_against: { master_film: [:formula], sales_order: [:code] }
+  pg_search_scope :search,
+                  against: [:serial, :note, :shelf, :phase],
+                  using: { tsearch: { prefix: true } },
+                  associated_against: { master_film: [:formula], sales_order: [:code] }
 
   def split
     master_film.create_film(phase, width, length)
@@ -75,14 +75,18 @@ class Film < ActiveRecord::Base
 
   def self.phase(phase, tenant = nil)
     case phase
-    when "lamination", "inspection", "stock", "reserved", "wip", "fg", "nc", "scrap"
-      active.send(phase)
-    when "large_stock"
-      active.stock.large(tenant.small_area_cutoff)
-    when "small_stock"
-      active.stock.small(tenant.small_area_cutoff)
-    when "deleted"
-      deleted
+      when "lamination", "inspection", "stock", "reserved", "wip", "fg", "nc", "scrap"
+        active.send(phase)
+      when "large_stock"
+        active.stock.large(tenant.small_area_cutoff)
+      when "small_stock"
+        active.stock.small(tenant.small_area_cutoff)
+      when "recent_fg"
+        active.fg.serial_date_after(1.year.ago)
+      when "archive_fg"
+        active.fg.serial_date_before(1.year.ago)
+      when "deleted"
+        deleted
     end
   end
 
@@ -98,9 +102,9 @@ class Film < ActiveRecord::Base
   end
 
   def record_movement(from, to, actor)
-    film_movements.create!(from_phase: from, 
-                           to_phase: to, 
-                           actor: actor.full_name, 
+    film_movements.create!(from_phase: from,
+                           to_phase: to,
+                           actor: actor.full_name,
                            width: width,
                            length: length,
                            tenant_code: tenant_code)
@@ -121,22 +125,22 @@ class Film < ActiveRecord::Base
 
   def valid_destinations
     case phase
-    when "lamination"
-      %w{inspection}
-    when "inspection"
-      %w{stock reserved wip nc}
-    when "stock"
-      %w{reserved wip nc}
-    when "reserved"
-      %w{wip stock nc}
-    when "wip"
-      %w{fg reserved stock nc}
-    when "fg"
-      %w{wip stock reserved nc}
-    when "nc"
-      %w{scrap stock reserved}
-    when "scrap"
-      %w{stock reserved nc}
+      when "lamination"
+        %w{inspection}
+      when "inspection"
+        %w{stock reserved wip nc}
+      when "stock"
+        %w{reserved wip nc}
+      when "reserved"
+        %w{wip stock nc}
+      when "wip"
+        %w{fg reserved stock nc}
+      when "fg"
+        %w{wip stock reserved nc}
+      when "nc"
+        %w{scrap stock reserved}
+      when "scrap"
+        %w{stock reserved nc}
     end
   end
 
@@ -149,7 +153,7 @@ class Film < ActiveRecord::Base
   end
 
   private
-  
+
   def upcase_shelf
     shelf.upcase! if shelf.present?
   end
