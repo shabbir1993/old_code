a5e95935c310ac0c5270f870eb568b417214af1f
polytronix <polytronix1@gmail.com>
2017-07-12 07:39:22 -0500

    Update master_film.rb
    Replace code with the first formula

--- app/models/master_film.rb
+++ app/models/master_film.rb
@@ -130,7 +130,7 @@ class MasterFilm < ActiveRecord::Base
 
   def set_yield
     if yieldable?
-      self.yield = (((((effective_width*effective_length)/144)/mix_mass)/machine.yield_constant)*100)
+      self.yield = 100*tenant.yield_multiplier*(effective_area/mix_mass)/machine.yield_constant
     else
       self.yield = nil
     end
